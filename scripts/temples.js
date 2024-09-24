8a437c10ca01c62d3345e5b3c9c3b47fc1591d30
GavinHBurch <burchyboy13@gmail.com>
2024-09-24 16:57:38 -0600

    week 2

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+const navigation = document.querySelector('.navigation')
+
+menu.addEventListener('click', () => {
+	
+	toggleMenuButton();
+});
+
+
+function toggleMenuButton() {
+	menu.classList.toggle('show');
+	layout.classList.toggle('show');
+};
